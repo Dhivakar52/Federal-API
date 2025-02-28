4144d50b82e4202889481716cf553f7404a579fb
TheFederalNews <awsstg@thefederal.in>
2025-02-28 11:27:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const summarizeRouter = require('./routes/summary');
 const flashRouter = require('./routes/flash');
 const peerRouter = require('./routes/peer');
 const newsRoutes = require('./routes/tubesrcipt');
+const privotRoutes = require('./routes/privot');
 const morgan = require('morgan');
 const punycode = require('punycode/');
 const app = express();
@@ -22,7 +23,7 @@ app.use('/summary', summarizeRouter);
 app.use('/', flashRouter);
 app.use('/peer', peerRouter);
 app.use('/api', newsRoutes);
-
+app.use('/privot', privotRoutes);
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
