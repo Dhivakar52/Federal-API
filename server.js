3d2e8b5134cf093b24963f6a98e8f491517925fe
TheFederalNews <awsstg@thefederal.in>
2025-06-24 22:58:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-const username = 'myUser';
-const password = 'myPass123';
+const username = 'dbadm';
+const password = 'SrmT!234';
 const dbName = 'employee';
 
 mongoose
